146f0f091d9d03170b3c25241f71c0a7cd7a2846
systemsgenomics <35600340+systemsgenomics@users.noreply.github.com>
2023-05-13 08:38:48 +0300

    Create cellranger-atac_script.sh

---
+++ analysis/preprocessing/cellranger-atac_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cellranger_atac=cellranger-atac-1.2.0/cellranger-atac
+ref=refdata-cellranger-atac-hg19-and-mm10-1.2.0
+fastq_path=data/scATACseq/raw_data/fastq/
+mem=360
+cores=40
+
+for i in {1..8}
+do
+        id=T$i
+
+        $cellranger_atac count --id=$id --reference=$ref --fastqs=$fastq_path$id --sample=$id --localcores=$vores --localmem=$mem --disable-ui
+done
